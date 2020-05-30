4950316018910a4d75771ea4da41704616ed4fd8
amirSohel007 <amirsohel27@gmail.com>
2020-05-30 16:17:13 +0530

    user profile page

--- src/App.js
+++ src/App.js
@@ -5,21 +5,17 @@ import Login from "./components/views/Login";
 import Register from "./components/views/Register";
 import Menu from "./components/views/Menu";
 import Home from "./components/views/Home";
+import UserProfile from "./components/views/User";
 
 function App() {
 	return (
 		<Router>
 			<Menu />
 			<Switch>
-				<Route path="/" exact>
-					<Home />
-				</Route>
-				<Route path="/login" exact>
-					<Login />
-				</Route>
-				<Route path="/register" exact>
-					<Register />
-				</Route>
+				<Route path="/" exact><Home /></Route>
+				<Route path="/login" exact><Login /></Route>
+				<Route path="/register" exact><Register /></Route>
+				<Route path="/user/:id" exact><UserProfile /></Route>
 			</Switch>
 		</Router>
 	);
---
+++ src/components/views/User.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import axios from "axios";
+
+const UserProfile = () => {
+    const [userDetail, setUserDetail] = useState('')
+    const apiURL = process.env.REACT_APP_API_URL
+
+    useEffect(async () => {
+        let res = await axios.get(`${apiURL}user/5ed238851f4618001780179b`);
+        setUserDetail(res.data)
+        console.log(res.data)
+    },[])
+    
+	return (
+		<Layout>
+			<div className="row justify-content-center">
+				<div className="col-sm-6 text-center">
+					<div className="bg-white form-wrapper mt-5 p-4 border-radius-4">
+                    {userDetail && userDetail ? <div>
+                            <h3>{userDetail.username}</h3>
+                            <p>Email : {userDetail.email}</p>
+                            <p>total posts {userDetail && userDetail.posts.length}</p>
+                            <p>total followers {userDetail.followers.length}</p>
+                            <p>total following {userDetail.following.length}</p>
+                    </div> : 'Loading...'}
+
+                           
+                    </div>
+				</div>
+			</div>
+		</Layout>
+	);
+};
+
+export default UserProfile;
